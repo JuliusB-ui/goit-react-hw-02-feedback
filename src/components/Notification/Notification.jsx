fcd89318a22cf7538069bc42c3555163167deaf1
JuliusB-ui <baraquiel.julius@gmail.com>
2024-06-17 01:05:04 +0700

    modifications

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,3 +1,10 @@
+import PropTypes from 'prop-types';
+import css from './Notification.module.css';
+
 export const Notification = ({ message }) => {
-  return <p>{message}</p>;
+  return <p className={css.warning}>{message}</p>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
 };
